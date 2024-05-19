9c630c54dc3b7955497c22aba98f706866fac788
Ashryz <tarek.ashry9894@outlook.com>
2024-05-19 20:02:37 +0300

    add security file and permissions

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,8 @@
     "description": "Oddo module for managing students and courses in iti",
     "depends": ["hr"],
     "data": [
+        "security/iti_security.xml",
+        "security/ir.model.access.csv",
         "views/iti_students_views.xml",
         "views/iti_tracks_views.xml",
         "views/iti_hr_views.xml"
